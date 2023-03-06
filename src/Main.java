c002e25a20826facb8dbfa995d8fc4e6a0acc038
BeniMalan <malanbeniaminflorin@gmail.com>
2023-03-06 20:11:25 +0200

    first comit from inteliJ

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 
 public class Main {
     public static void main(String[] args){
-    System.out.println("Hello world!");
+    System.out.println("Thi is experiment");
     System.out.println("Ma numesc Beni");
 
   }
